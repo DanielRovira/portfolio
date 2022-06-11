e208970230ea988e027234d84f7317693babaac6
DanielRovira <danielroviratorres@gmail.com>
2022-06-11 15:49:52 -0300

    main

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,13 @@ import initTyping from "./modules/typing.js";
 
 
 setTimeout(initTyping, 500);
+
+const checkbox = document.getElementById('close-menu')
+
+checkbox.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    document.documentElement.style.overflowY='hidden';
+  } else {
+    document.documentElement.style.overflowY='auto';
+  }
+})
