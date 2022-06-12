349f83dfb9ebd6bb1b5068a852dff187d9de9b2d
danielroviratorres@gmail.com <danielroviratorres@gmail.com>
2022-06-12 00:34:19 -0300

    scroll

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,27 @@ checkbox.addEventListener('change', (event) => {
     document.documentElement.style.overflowY='auto';
   }
 })
+
+
+var onAppear = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+  onAppear = [].map.call(document.querySelectorAll(".onAppear"), function(item ) {
+    return item;
+  });
+}, false);
+
+window.addEventListener("scroll", function() {
+  onAppear.forEach(function(elem) {
+    var vwTop = window.pageYOffset;
+    var vwBottom = (window.pageYOffset + window.innerHeight);
+    var elemTop = elem.offsetTop;
+    var elemHeight = elem.offsetHeight;
+    
+    if (vwBottom > elemTop && ((vwTop - elemHeight) < elemTop)) {
+     elem.classList.add("visible");
+    } else {
+      elem.classList.remove("visible");
+    }
+  });
+}, false);
