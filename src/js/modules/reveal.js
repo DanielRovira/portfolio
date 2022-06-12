aa14e6e37e5b8ae6e8debeb68111302a7e0c6580
danielroviratorres@gmail.com <danielroviratorres@gmail.com>
2022-06-11 22:50:11 -0300

    s

---
+++ src/js/modules/reveal.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+
+sr.reveal('.home-left', {duration : 3000, origin: 'bottom'})
+sr.reveal('.home-right', {duration : 3000,  origin: 'bottom'})
+sr.reveal('.skills', {duration : 3000,  origin: 'bottom'})
+sr.reveal('.projects', {duration : 3000, origin:'bottom'})
+
