bf9725955e66a55419619821b07ca65c5ae1114a
DanielRovira <danielroviratorres@gmail.com>
2022-06-14 11:40:07 -0300

    a

--- src/js/modules/about.js
+++ src/js/modules/about.js
@@ -8,7 +8,7 @@ const buttons = document.querySelectorAll('.about-btn');
 
 buttons.forEach(  (selected) => {
     const idSelected = selected.attributes.id.value
-    selected.addEventListener('mouseover', () => {
+    selected.addEventListener('click', () => {
         document.querySelector('.about-text').innerHTML = about[idSelected];
         
         buttons.forEach(button => {
