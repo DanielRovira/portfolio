4036c887a9126666054b8f4dfd838e200922c61f
danielroviratorres@gmail.com <danielroviratorres@gmail.com>
2022-06-25 13:31:40 -0300

    Skills additions

--- src/js/text.js
+++ src/js/text.js
@@ -7,12 +7,15 @@ professional: "Bachelor and Master in Ecology.  In my academic career I used a l
 }
 
 const Skill = {
-html:   '<p>HTML</p> <br> <p>It is a markup language used in building web pages.</p> <br>',
-css:    '<p>CSS</p> <br> <p>It is a mechanism for adding style to a web document.</p> <br>',
-js:     '<p>JavaScript</p> <br> <p>It is a programming language. Along with HTML and CSS, it is one of the top three technologies on the web.</p> <br>',
-git:    '<p>Git</p> <br> <p>It is a distributed version control system, mainly used in software development.</p> <br>',
-github: '<p>Github</p> <br> <p>It is a source code and file hosting platform with version control using Git.</p> <br>',
-hover:  '/* Hover over a skill to read the description */'
+html:   "<p>HTML</p> <br> <p>It is a markup language used in building web pages.</p> <br>",
+css:    "<p>CSS</p> <br> <p>It is a mechanism for adding style to a web document.</p> <br>",
+js:     "<p>JavaScript</p> <br> <p>It is a programming language. Along with HTML and CSS, it is one of the top three technologies on the web.</p> <br>",
+react:  "<p>React.js<p> <br> It is a front-end JavaScript library for building user interfaces based on UI components.",
+native: "<p>React Native<p> <br> It is used to develop mobile applications (Android, Android TV, iOS,macOS, tvOS, Web, Windows and UWP) by enabling developers to use the React framework along with native platform capabilities.",
+nodejs: "<p>Node.js<p> <br> It is a back-end JavaScript runtime environment that executes JavaScript code outside a web browser. Node.js lets developers use JavaScript to write command line tools and for server-side scripting—running.",
+git:    "<p>Git</p> <br> <p>It is a distributed version control system, mainly used in software development.</p> <br>",
+github: "<p>Github</p> <br> <p>It is a source code and file hosting platform with version control using Git.</p> <br>",
+hover:  "/* Hover over a skill to read the description */"
 }
 
 export {typeName, about, Skill}
